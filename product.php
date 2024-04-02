883972ed7969ea4ceffde8236fb4fc62567378e6
EudiK <khanhallok@gmail.com>
2024-04-02 23:14:28 +0700

    html filter

--- product.php
+++ product.php
@@ -1,35 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <title>Product</title>
 </head>
+
 <body>
-        <div class="container">
-            <div class="row">
-                <div class="col">1</div>
-                <div class="col">2</div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <button type="button" class="btn btn-primary btn-sm">Add product</button>
+                <button type="button" class="btn btn-primary btn-sm">Add property</button>
+                <button type="button" class="btn btn-primary btn-sm">Sync from VillaTheme</button>
             </div>
-            <div class="row">
-
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    
+            <div class="col">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search product"
+                        aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                </div>
             </div>
         </div>
+        <div class="row">
+
+            <div class="col">
+
+                <select class="form-select" id="" >
+                    <option selected>Date</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+
+            </div>
+
+            <div class="col">
+
+                <select class="form-select" id="" >
+                    <option selected>ASC</option>
+                    <option value="1">DESC</option>
+                    
+                </select>
+
+            </div>
+
+            <div class="col">
+
+                <select class="form-select" id="" >
+                    <option selected>Category</option>
+                    <option value="1">One</option>
+                    
+                </select>
+
+            </div>
+
+            <div class="col">
+
+                <select class="form-select" id="" >
+                    <option selected>Select tag</option>
+                    <option value="1">One</option>
+                    
+                </select>
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+            </div>
+            <div class="col">
+
+                <input type="date" id="" class="form-control" >
+                
+            </div>
+
+            <div class="col">
+            <input type="date" id="" class="form-control" >
+            </div>
+            <div class="col">
+            <input type="text" id="" class="form-control" placeholder="price from">
+            </div>
+            <div class="col">
+            <input type="text" id="" class="form-control" placeholder="price to">
+            </div>
+            <div class="col">
+            <button type="button" class="btn btn-primary btn-sm">Small button</button>
+            </div>
+
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
