f9814ab04100ab8be55f17de901f24521c32e09c
EudiK <khanhallok@gmail.com>
2024-04-09 01:03:04 +0700

    9/4

--- JavaScript/validator.js
+++ JavaScript/validator.js
@@ -91,7 +91,7 @@ validator.isChooseOne = function (selector) {
         test: function (value) {
             let anySelect = document.querySelector(selector).querySelector('.select2-selection__choice')
             console.log(anySelect)
-            return anySelect ? "Vui chọn một!" : undefined
+            return anySelect ? "Vui lòng chọn một!" : undefined
         }
     }
 } 
@@ -100,8 +100,20 @@ validator.onlyText = function (selector) {
     return {
         selector: selector,
         test: function (value) {
-            let regex = /^[a-zA-ZÃ€ÃÃ‚ÃƒÃˆÃ‰ÃŠÃŒÃÃ’Ã“Ã”Ã•Ã™ÃšÄ‚ÄÄ¨Å¨Æ Ã Ã¡Ã¢Ã£Ã¨Ã©ÃªÃ¬Ã­Ã²Ã³Ã´ÃµÃ¹ÃºÄƒÄ‘Ä©Å©Æ¡Æ¯Ä‚áº áº¢áº¤áº¦áº¨áºªáº¬áº®áº°áº²áº´áº¶áº¸áººáº¼á»€á»€á»‚áº¾Æ°Äƒáº¡áº£áº¥áº§áº©áº«áº­áº¯áº±áº³áºµáº·áº¹áº»áº½á»á»á»ƒáº¿á»„á»†á»ˆá»Šá»Œá»Žá»á»’á»”á»–á»˜á»šá»œá»žá» á»¢á»¤á»¦á»¨á»ªá»…á»‡á»‰á»‹á»á»á»‘á»“á»•á»—á»™á»›á»á»Ÿá»¡á»£á»¥á»§á»©á»«á»¬á»®á»°á»²á»´Ãá»¶á»¸á»­á»¯á»±á»³á»µá»·á»¹\s\W|_]+$/;
-            return regex.test(value.trim()) == "" ? "Chỉ nhập chữ" : undefined
+            let regex = /^[a-z0-9A-ZÃ€ÃÃ‚ÃƒÃˆÃ‰ÃŠÃŒÃÃ’Ã“Ã”Ã•Ã™ÃšÄ‚ÄÄ¨Å¨Æ Ã Ã¡Ã¢Ã£Ã¨Ã©ÃªÃ¬Ã­Ã²Ã³Ã´ÃµÃ¹ÃºÄƒÄ‘Ä©Å©Æ¡Æ¯Ä‚áº áº¢áº¤áº¦áº¨áºªáº¬áº®áº°áº²áº´áº¶áº¸áººáº¼á»€á»€á»‚áº¾Æ°Äƒáº¡áº£áº¥áº§áº©áº«áº­áº¯áº±áº³áºµáº·áº¹áº»áº½á»á»á»ƒáº¿á»„á»†á»ˆá»Šá»Œá»Žá»á»’á»”á»–á»˜á»šá»œá»žá» á»¢á»¤á»¦á»¨á»ªá»…á»‡á»‰á»‹á»á»á»‘á»“á»•á»—á»™á»›á»á»Ÿá»¡á»£á»¥á»§á»©á»«á»¬á»®á»°á»²á»´Ãá»¶á»¸á»­á»¯á»±á»³á»µá»·á»¹\s\W|_]+$/;
+            return regex.test(value.trim()) ? "Chỉ nhập chữ" : undefined
+        }
+    }
+}
+
+validator.isImage = function (selector){
+    return {
+        selector: selector,
+        test: function (value) {
+            let inputFile = document.querySelector(selector).value
+            console.log(inputFile)
+            let regex = /[\/.](gif|jpg|jpeg|tiff|png)$/i;
+            return regex.test(inputFile) ? "Vui chọn ảnh!" : undefined
         }
     }
 }
